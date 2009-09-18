e1a1d7b55292e2a1b85a457c6b5d68e6b47bf85d
spadgeht <spadgeht@f630f2fe-a1b0-11de-b04a-1faa45bc5d20>
2009-09-18 20:10:39 +0000

    Implementacion de valores a las cartas cada que roban

--- trunk/src/BlackJackPlayer.java
+++ trunk/src/BlackJackPlayer.java
@@ -1,28 +1,25 @@
 import cardgame.Card;
 import cardgame.Hand;
+import cardgame.Rank;
 
 
 public abstract class BlackJackPlayer {
 
 	private BlackJackHand hand;
-	private int acumValue;
-	
+
 	public BlackJackPlayer() {
 		hand = new BlackJackHand();	
-		acumValue = 0;
 	}
 	
 	public BlackJackHand getHand(){
 		return hand;
 	}
 	
-	public void hit(Card card){
+	public int hit(Card card){
 		hand.addCard(card);
+		return hand.evaluateHand();
 	}
 	
-	public int getTotal(){
-		return acumValue;
-	}
 	
 	/*
 	public int scoreKingHigh(){
@@ -34,10 +31,40 @@ public abstract class BlackJackPlayer {
 	*/
 	
 	private class BlackJackHand extends Hand{
-
+		
+		private int acumValue;
+		private int acesHigh;
+		
+		public BlackJackHand(){
+			acumValue = 0;
+			acesHigh=0;
+		}
+		
 		@Override
 		public int evaluateHand() {
-			return 0;
+			Card c = getCard(getNumberOfCards()-1);
+			if (c.getRank().compareTo(Rank.ACE) == 0){
+				acumValue +=11;
+				if (acumValue > 21){
+					acumValue-=10;
+				} else {
+					acesHigh++; 
+				}
+			}
+			else if (c.getRank().compareTo(Rank.JACK) < 0){
+				acumValue +=10;
+				if (acumValue > 21 && acesHigh>0){
+					acesHigh--;
+				}
+			}
+			else if (c.getRank().compareTo(Rank.TEN) > 0){
+				acumValue+=10;
+				if (acumValue > 21 && acesHigh>0){
+					acesHigh--;
+				}
+			}
+			
+			return acumValue;
 		}
 		
 	}
